0b2060ddb88c1d54d4aa54a689d9948cd326c630
github-action-benchmark <github@users.noreply.github.com>
2022-12-21 00:56:02 +0000

    add Benchmark (benchmarkjs) benchmark result for 34f05ecf3c05ba61ca1e520a2c36c6f5a9a49809

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1671583928121,
+  "lastUpdate": 1671584162058,
   "repoUrl": "https://github.com/MrDesjardins/all-in-throttle-out-queue",
   "entries": {
     "Benchmark": [
@@ -31,6 +31,37 @@ window.BENCHMARK_DATA = {
             "range": "±0.16%",
             "unit": "ops/sec",
             "extra": "90 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mrdesjardins@gmail.com",
+            "name": "Patrick",
+            "username": "MrDesjardins"
+          },
+          "committer": {
+            "email": "mrdesjardins@gmail.com",
+            "name": "Patrick",
+            "username": "MrDesjardins"
+          },
+          "distinct": true,
+          "id": "34f05ecf3c05ba61ca1e520a2c36c6f5a9a49809",
+          "message": "Update action to v3 to remove warnings",
+          "timestamp": "2022-12-20T16:55:08-08:00",
+          "tree_id": "b275788ffeff35861721549d513e059e2047a8e4",
+          "url": "https://github.com/MrDesjardins/all-in-throttle-out-queue/commit/34f05ecf3c05ba61ca1e520a2c36c6f5a9a49809"
+        },
+        "date": 1671584161418,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "addMessageIntoBuffer Small Message",
+            "value": 97.82,
+            "range": "±0.19%",
+            "unit": "ops/sec",
+            "extra": "81 samples"
           }
         ]
       }
