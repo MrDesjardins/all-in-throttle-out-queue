5ffd6953f9a9a80e216f1054945b6ec6dbb69ab0
github-action-benchmark <github@users.noreply.github.com>
2022-12-21 00:52:08 +0000

    add Benchmark (benchmarkjs) benchmark result for 46ccd04f82574e0993ac170208a30966413e9e50

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1671583928121,
+  "repoUrl": "https://github.com/MrDesjardins/all-in-throttle-out-queue",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "mrdesjardins@gmail.com",
+            "name": "Patrick",
+            "username": "MrDesjardins"
+          },
+          "committer": {
+            "email": "mrdesjardins@gmail.com",
+            "name": "Patrick",
+            "username": "MrDesjardins"
+          },
+          "distinct": true,
+          "id": "46ccd04f82574e0993ac170208a30966413e9e50",
+          "message": "First commit",
+          "timestamp": "2022-12-20T16:45:08-08:00",
+          "tree_id": "faaf23b24aa21228523110c3e5a07e6199a61768",
+          "url": "https://github.com/MrDesjardins/all-in-throttle-out-queue/commit/46ccd04f82574e0993ac170208a30966413e9e50"
+        },
+        "date": 1671583927628,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "addMessageIntoBuffer Small Message",
+            "value": 97.88,
+            "range": "±0.16%",
+            "unit": "ops/sec",
+            "extra": "90 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
